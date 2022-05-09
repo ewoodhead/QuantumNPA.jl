054794a874b351b4589363ef9e5a1b0de202b4bc
Erik Woodhead <erik.woodhead@ulb.be>
2022-05-09 17:23:35 +0200

    add file

---
+++ QuantumNPA.jl
@@ -0,0 +1,10 @@
+module QuantumNPA
+
+export npa_min, npa_max,
+    @dichotomic,
+    dichotomic, fourier, unitary, projector, zbff,
+    Monomial, Polynomial, monomials, coefficients
+
+include("qnpa.jl")
+
+end
