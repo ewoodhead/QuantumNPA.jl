b4792d962af22591ccf51a8a1260c5481c92bce5
Erik Woodhead <erik.woodhead@ulb.be>
2022-05-31 15:18:30 +0200

    export _d functions

--- QuantumNPA.jl
+++ QuantumNPA.jl
@@ -1,6 +1,7 @@
 module QuantumNPA
 
 export npa_min, npa_max, npa2sdp, sdp2jump, npa2jump,
+    npa_mind, npa_maxd, sdp2jumpd, npa2jumpd,
     @dichotomic,
     dichotomic, fourier, unitary, projector, zbff,
     Monomial, Polynomial, monomials, coefficients,
