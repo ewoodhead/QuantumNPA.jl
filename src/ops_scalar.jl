a4df10d99c9509f377e8a44a60d7e4176527fd15
Erik Woodhead <erik.woodhead@ulb.be>
2022-09-08 19:15:56 +0200

    fixet bra-ket products

--- src/ops_scalar.jl
+++ src/ops_scalar.jl
@@ -12,6 +12,9 @@ end
 
 Base.hash(x::Trace, h::UInt) = hash(x.word, h)
 
+Base.conj(x::Trace) = Trace(conj_pol(x.word))
+Base.adjoint(x::Trace) = conj(x)
+
 function ctrace(m::Monomial)
     coeff = 1
 
@@ -28,22 +31,58 @@ end
 
 
 
-struct Ket
-    label
-end
-
-struct Bra
-    label
-end
+abstract type Ket end
+abstract type Bra end
 
 Base.show(io::IO, x::Ket) = print(io, '|', x.label, '>')
 Base.show(io::IO, x::Bra) = print(io, '<', x.label, '|')
 
-Base.conj(x::Ket) = Bra(x.label)
-Base.conj(x::Bra) = Ket(x.label)
-
 Base.hash(x::Ket, h::UInt) = hash(x.label, h)
 Base.hash(x::Bra, h::UInt) = hash(x.label, h)
+
+Base.adjoint(x::Ket) = conj(x)
+
+struct AKet <: Ket
+    label
+end
+
+struct ABra <: Bra
+    label
+end
+
+Base.conj(x::AKet) = ABra(x.label)
+Base.conj(x::ABra) = AKet(x.label)
+
+struct OKet <: Ket
+    label
+    gram::Dict
+end
+
+struct OBra <: Bra
+    label
+    gram::Dict
+end
+
+Base.conj(x::OKet) = OBra(x.label, x.gram)
+Base.conj(x::OBra) = OKet(x.label, x.gram)
+
+function orthonormal_states(labels)
+    gram = Dict(l => 1 for l in labels)
+    return [OKet(l, gram) for l in labels]
+end
+
+struct GKet <: Ket
+    label
+    gram::Dict
+end
+
+struct GBra <: Bra
+    label
+    gram::Dict
+end
+
+Base.conj(x::GKet) = GBra(x.label, x.gram)
+Base.conj(x::GBra) = GKet(x.label, x.gram)
 
 
 
@@ -57,11 +96,11 @@ struct CState
     word::PartiedOpList
 end
 
-ket(label) = State(PartiedOpList[], Ket(label)) 
-bra(label) = CState(Bra(label), PartiedOpList[])
+state(label) = State(PartiedOpList[], AKet(label))
+cstate(label) = CState(ABra(label), PartiedOpList[])
 
 if !@isdefined(Psi)
-    const Psi = ket(:Ψ)
+    const Psi = AKet(:Ψ)
 end
 
 function Base.show(io::IO, x::State)
@@ -77,6 +116,9 @@ end
 Base.conj(x::State) = CState(conj(x.ket), conj_pol(x.word))
 Base.conj(x::CState) = State(conj_pol(x.word), conj(x.bra))
 
+Base.adjoint(x::State) = conj(x)
+Base.adjoint(x::CState) = conj(x)
+
 Base.hash(x::State, h::UInt) = hash((x.word, x.ket), h)
 Base.hash(x::CState, h::UInt) = hash((x.bra, x.word), h)
 
@@ -87,6 +129,42 @@ struct BraKet
     word::PartiedOpList
     ket::Ket
 end
+
+braket(x::Bra, y::Ket) = BraKet(x, PartiedOpList[], y)
+
+Base.:*(x::Bra, y::Ket) = braket(x, y)
+
+function Base.:*(x::OBra, y::OKet)
+    gram = x.gram
+
+    if (y.gram === gram)
+        l = x.label
+
+        if (y.label != l)
+            return 0
+        elseif haskey(gram, l)
+            return gram[l]
+        end
+    end
+
+    return braket(x, y)
+end
+
+function Base.:*(x::GBra, y::GKet)
+    gram = x.gram
+
+    if (y.gram === gram)
+        pair = (x.label, y.label)
+
+        if haskey(gram, pair)
+            return gram[pair]
+        end
+    end
+
+    return braket(x, y)
+end
+
+
 
 function Base.show(io::IO, x::BraKet)
     word = x.word
@@ -102,6 +180,7 @@ function Base.show(io::IO, x::BraKet)
 end
 
 Base.conj(x::BraKet) = BraKet(conj(x.ket), conj_pol(x.word), conj(x.bra))
+Base.adjoint(x::BraKet) = conj(x)
 Base.hash(x::BraKet, h::UInt) = hash((x.bra, x.word, x.ket), h)
 
 function Base.:*(x::CState, y::State)
