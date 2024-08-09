6fcf2d5d60130a36f1ca157957ef349d379ececd
Erik Woodhead <erik.woodhead@quantinuum.com>
2024-08-09 13:16:23 +0100

    export hermitian

--- src/QuantumNPA.jl
+++ src/QuantumNPA.jl
@@ -1,9 +1,9 @@
 module QuantumNPA
 
 export @dichotomic, @pauli, Id, Monomial, Polynomial, acomm, cglmp,
-    coefficients, comm, dichotomic, direct_sum, fourier, generic, isidentity,
-    monomials, npa2jump, npa2sdp, npa_level, npa_max, npa_min, npa_moment,
-    operators, pauli, projector, sdp2jump, set_solver!, unitary
+    coefficients, comm, dichotomic, direct_sum, fourier, hermitian, generic,
+    isidentity, monomials, npa2jump, npa2sdp, npa_level, npa_max, npa_min,
+    npa_moment, operators, pauli, projector, sdp2jump, set_solver!, unitary
 
 using Base.Iterators #  flatten, zip (filter?)
 using BlockDiagonals
